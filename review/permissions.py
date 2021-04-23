9f69b51f61b6fd70595f17635d2bd7d56b26effc
Николай <slonopatam52@gmail.com>
2021-04-23 15:16:49 +0300

    Add views, urls and smt fixes

--- review/permissions.py
+++ review/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsOwnerAdminModeratorToEdit(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        if request.user.role in (["moderator", "admin"]
-                                 or request.user.is_superuser):
-            return True
-        return request.user == obj.author
+        return (
+            request.method in SAFE_METHODS
+            or request.user.role in (["moderator", "admin"])
+            or request.user.is_superuser
+            or request.user == obj.author
+        )
---
+++ review/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import ReviewsViewSet, CommentsViewSet
+
+
+router = routers.DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewsViewSet,
+    basename='ReviewsViewSet')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet,
+    basename='CommentsViewSet')
+
+
+urlpatterns = path('v1/', include(router.urls))
