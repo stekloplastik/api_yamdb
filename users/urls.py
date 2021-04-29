216f863c30432db560253637952e5a39f86e58ea
Vlada Mylnikova <vlada.mylnikova@gs-labs.ru>
2021-04-29 19:06:53 +0200

    fix tests users/me

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CreateUserViewSet, get_jwt_token, send_confirmation_code,
-                    UserEdit)
+from .views import CreateUserViewSet, get_jwt_token, send_confirmation_code
 
 router = DefaultRouter()
 router.register('users', CreateUserViewSet)
-router.register('users/me', UserEdit)
 
 urlpatterns = [
     path('auth/email/', send_confirmation_code),
